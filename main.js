185a17e9c5e3b42dd3e0afb29bab9f044e0b22b7
Darek <gawlikdaarek@gmail.com>
2022-11-11 10:14:44 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('../books.json') //path to the file with json data
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        });
+
